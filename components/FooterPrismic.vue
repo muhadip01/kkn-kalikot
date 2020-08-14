d9a3545a517086f649878f98aee482cbaf4b1098
muhadip01 <muhammad_adixzdmx@yahoo.com>
2020-08-14 19:15:13 +0700

    footer update
    footer + logo and background

--- components/FooterPrismic.vue
+++ components/FooterPrismic.vue
@@ -1,7 +1,14 @@
 <template>
-  <footer class="footer">
-    <h2>Kecamatan Kalikotes</h2>
-  </footer>
+  <div class="bg">
+    <footer class="footer">
+     <div class="foothead">
+        <nuxt-link to="/" class="logo">
+          <prismic-image :field="$store.state.menu.image"/>
+        </nuxt-link>
+      <h2>Kecamatan Kalikotes</h2>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -15,7 +22,12 @@ export default {
   padding: 40px 0
   margin: 0 auto
   text-align: center
-  border-top: 1px solid #DADADA
+  border-top: 1px solid
+
+.bg
+  width: calc(100% + 40px)
+  background: #b3fff5
+  margin: 0 -20px -20px
 
 .logo
   width: 100px
--- layouts/homepage.vue
+++ layouts/homepage.vue
@@ -35,6 +35,7 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-bottom: 50px
 }
 .gmaps{
   
